317fd9c42e22c364ae1a0a47b4fea024fd34c63d
Stanley Huang <shuang50@stuy.edu>
2024-11-26 13:19:58 -0600

    working children

--- fork.c
+++ fork.c
@@ -5,21 +5,21 @@
 #include <fcntl.h>
 int main(){
   int pid = getpid();
-  printf("%d about to create 2 child processes", pid);
+  printf("%d about to create 2 child processes\n", pid);
   pid_t a, b;
   fork();
   (a = fork()) && (b = fork());
-  if (a == 0 || b == 0){
+  if (a == 0){
     int* bob = malloc(sizeof(int));
     int random = open("/dev/urandom", O_RDONLY);
     int joe = read(random, bob, sizeof(int));
-    int bob1 = bob;
-    bob = bob % 5;
-    printf("%d %dsec", getpid(), bob);
-    sleep(bob);
-    printf("%d finished after %d second(s)", getpid(), bob);
+    int bob1 = bob[0] % 4 + 1;
+    free(bob);
+    printf("%d %dsec\n", getpid(), bob1);
+    sleep(bob1);
+    printf("%d finished after %d second(s)\n", getpid(), bob1);
   }
   else{
-    wait();
+    wait(NULL);
   }
 }
