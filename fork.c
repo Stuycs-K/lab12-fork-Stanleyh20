eae5252664e3fc3603095c9ddeb37ba9e503791e
Stanley huang <shuang50@stuy.edu>
2024-11-26 00:23:52 -0500

    wrote some unworking code

--- fork.c
+++ fork.c
@@ -2,11 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-
+#include <fcntl.h>
 int main(){
-  int a = getpid();
+  int pid = getpid();
+  printf("%d about to create 2 child processes", pid);
+  pid_t a, b;
   fork();
-  if (a == getpid()){
-    fork();
+  (a = fork()) && (b = fork());
+  if (a == 0 || b == 0){
+    int* bob = malloc(sizeof(int));
+    int random = open("/dev/urandom", O_RDONLY);
+    int joe = read(random, bob, sizeof(int));
+    int bob1 = bob;
+    bob = bob % 5;
+    printf("%d %dsec", getpid(), bob);
+    sleep(bob);
+    printf("%d finished after %d second(s)", getpid(), bob);
+  }
+  else{
+    wait();
   }
 }
