64011984d2de94abc57cb9b7a66d34f4c294465d
Stanley Huang <shuang50@stuy.edu>
2024-11-26 17:55:45 -0500

    working everything

--- fork.c
+++ fork.c
@@ -11,7 +11,7 @@ int err(){
     exit(1);
 }
 int main(){
-  int *bob1 = malloc(sizeof(int));
+  int bob1 = 0;
   int pid = getpid();
   printf("%d about to create 2 child processes\n", pid);
   pid_t a, b;
@@ -20,17 +20,21 @@ int main(){
     int* bob = malloc(sizeof(int));
     int random = open("/dev/urandom", O_RDONLY);
     int joe = read(random, bob, sizeof(int));
-    bob1[0] = (bob[0] % 5 + 4) % 5 + 1;
+    bob1 = (bob[0] % 5 + 4) % 5 + 1;
     free(bob);
-    printf("%d %dsec\n", getpid(), bob1[0]);
-    sleep(bob1[0]);
-    printf("%d finished after %d second(s)\n", getpid(), bob1[0]);
+    printf("%d %dsec\n", getpid(), bob1);
+    sleep(bob1);
+    printf("%d finished after %d second(s)\n", getpid(), bob1);
+    return bob1;
   }
   else{
-    int child = wait(NULL);
+    int *status = malloc(sizeof(int));
+    int child = wait(status);
     if (child == -1){
       err();
     }
-    printf("Main Process %d is done. Child %d slept for %d sec\n", pid, child, bob1[0]);
+    if (WIFEXITED(*status)){
+    printf("Main Process %d is done. Child %d slept for %d sec\n", pid, child, WEXITSTATUS(*status));
+    }
   }
 }
